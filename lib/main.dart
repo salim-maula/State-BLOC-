26594eb0aadbca369e051db303a3c5a3ead0641a
salim-maula <huzaifah956@gmail.com>
2025-02-06 11:07:03 +0700

    Section 1-2 Basic Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//! cara menggunakan cubit
+class CounterCubit extends Cubit<int> {
+  //! bisa menggunakan constructor seperti ini atau seperti di bawahnnya
+  // CounterCubit(super.initialState);
+  CounterCubit() : super(0);
+
+  //! jika tidak mau 0, kita juga bisa set initialData, dengan melemparnya di dalam constructor
+  // CounterCubit({this.initialData = 0}) : super(initialData); //! ganti constructor diatas dengan yang ada di bawah
+  // int initialData;
+
+  void addData() {
+    emit(state + 1);
+  }
+
+  void minusData() {
+    emit(state - 1);
+  }
+}
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  Stream<int> countStream() async* {
-    for (var i = 0; i <= 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      yield i;
-    }
-  }
+  CounterCubit mycounter = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
@@ -37,25 +51,26 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           StreamBuilder(
-            stream: countStream(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: Text(
-                    "Loading",
-                    style: TextStyle(fontSize: 50),
-                  ),
-                );
-              } else {
-                return Center(
-                  child: Text(
-                    '${snapshot.data}',
-                    style: TextStyle(fontSize: 50),
-                  ),
-                );
-              }
-            },
-          ),
+              //! jika tidak mau menggunakan if == ConnectionState.waiting gunakan initial data
+              // initialData: mycounter.initialData,
+              stream: mycounter.stream,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: Text(
+                      'Loading',
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: Text(
+                      '${snapshot.data}',
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  );
+                }
+              }),
           const SizedBox(
             height: 20,
           ),
@@ -63,11 +78,15 @@ class HomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  mycounter.minusData();
+                },
                 icon: const Icon(Icons.remove),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  mycounter.addData();
+                },
                 icon: const Icon(Icons.add),
               ),
             ],
