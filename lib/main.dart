df0fc9ec7c57ee38666d4e751e592cabba642b39
salim-maula <huzaifah956@gmail.com>
2025-03-05 14:13:13 +0700

    Section 4-19 General Observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_bloc/general_observer.dart';
 
 import './pages/home.dart';
 
 void main() {
+  Bloc.observer = MyObserver();
   runApp(MyApp());
 }
 
